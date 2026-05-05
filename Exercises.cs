b117e35921d5e8fef8b6dcb19d5f867a4349f47f
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 15:31:37 +0200

    prep: add four menu option classes

---
+++ Exercises.cs
@@ -0,0 +1,25 @@
+namespace FlowControl;
+
+public class SingleTicketExercise : IExcercise
+{
+    public string Title => "Single Ticket Price calculator";
+    public void Run() => throw new System.NotImplementedException();
+}
+
+public class GroupTicketExercise : IExcercise
+{
+    public string Title => "Group Ticket Price calculator";
+    public void Run() => throw new System.NotImplementedException();
+}
+
+public class RepeatedTicketExercise : IExcercise
+{
+    public string Title => "Text Repeater";
+    public void Run() => throw new System.NotImplementedException();
+}
+
+public class ThirdWordExercise : IExcercise
+{
+    public string Title => "What's the Word?";
+    public void Run() => throw new System.NotImplementedException();
+}
