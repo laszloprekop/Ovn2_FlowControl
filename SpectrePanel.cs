e22a17c0fbde6f0209cba49882a1ce0d6226d8df
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 21:04:26 +0200

    fix: correct SpectreHeader class structure

--- SpectrePanel.cs
+++ SpectrePanel.cs
@@ -2,27 +2,24 @@ using Spectre.Console;
 
 namespace Ovn2_FlowControl;
 
-public class SpectrePanel
+class SpectreHeader : IViewFragment
 {
-    class SpectreHeader : IViewFragment
+    private readonly string _title;
+    private readonly string _description;
+
+    public SpectreHeader(string title, string description)
     {
-        private readonly string _title;
-        private readonly string _description;
-        
-        public SpectreHeader(string title, string description)
-        {
-            _title = title;
-            _description = description;
-        }
-        
-        public void Render()
-        {
-            AnsiConsole.Write(
-                new Panel(Markup.Escape(_description))
-                {
-                    Header = new PanelHeader(Markup.Escape(_title))
-                });
-            AnsiConsole.WriteLine();
-        }
+        _title = title;
+        _description = description;
+    }
+
+    public void Render()
+    {
+        AnsiConsole.Write(
+            new Panel(Markup.Escape(_description))
+            {
+                Header = new PanelHeader(Markup.Escape(_title))
+            });
+        AnsiConsole.WriteLine();
     }
 }
