a8383f30838120de974879e5788dcc86f9396286
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 16:14:07 +0200

    refactor: move single ticket price logic to SingleTicketExercise class

--- Program.cs
+++ Program.cs
@@ -46,30 +46,6 @@ namespace Ovn2_FlowControl
             }
         }
 
-        static void UngdomEllerPensionar()
-        {
-            Console.Write("Ange ålder: ");
-            string? input = Console.ReadLine();
-
-            if (!int.TryParse(input, out int alder)) // Jämför med int.Parse(input) --> "hej" --> Exception
-            {
-                Console.WriteLine("Ogiltig ålder.");
-                return;
-            }
-
-            if (alder < 20)
-            {
-                Console.WriteLine("Ungdomspris: 80kr");
-            }
-            else if (alder > 64)
-            {
-                Console.WriteLine("Pensionärspris: 90kr");
-            }
-            else
-            {
-                Console.WriteLine("Standardpris: 120kr");
-            }
-        }
 
         static void PrisForSallskap()
         {
--- SingleTicketExercise.cs
+++ SingleTicketExercise.cs
@@ -1,7 +1,33 @@
+using System;
+
 namespace Ovn2_FlowControl;
 
 public class SingleTicketExercise : IExercise
 {
     public string Title => "Single Ticket Price calculator";
-    public void Run() => throw new System.NotImplementedException();
+
+    public void Run()
+    {
+        Console.Write("Ange ålder: ");
+        string? input = Console.ReadLine();
+
+        if (!int.TryParse(input, out int alder)) // Jämför med int.Parse(input) --> "hej" --> Exception
+        {
+            Console.WriteLine("Ogiltig ålder.");
+            return;
+        }
+
+        if (alder < 20)
+        {
+            Console.WriteLine("Ungdomspris: 80kr");
+        }
+        else if (alder > 64)
+        {
+            Console.WriteLine("Pensionärspris: 90kr");
+        }
+        else
+        {
+            Console.WriteLine("Standardpris: 120kr");
+        }
+    }
 }
