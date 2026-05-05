22c6f7deca2a0b1d308e26852776a8021e200f40
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 20:38:18 +0200

    feat: add Header fragment to excercises

--- Menu.cs
+++ Menu.cs
@@ -12,11 +12,12 @@ public class Menu(List<IExercise> exercises)
         while (running)
         {
             Console.WriteLine("Välkommen till huvudmenyn.");
-            Console.WriteLine("[0] →  Avsluta");
 
             for (int i = 0; i < exercises.Count; i++)
                 Console.WriteLine($"[{i + 1}] →  {exercises[i].Title}");
 
+            Console.WriteLine();
+            Console.WriteLine($"Välj övning eller [0] →  Avsluta");
             Console.Write("> ");
 
             if (!int.TryParse(Console.ReadLine(), out int choice))
@@ -29,10 +30,15 @@ public class Menu(List<IExercise> exercises)
                     Console.WriteLine("Programmet avslutas.");
                     break;
                 case >= 1 when choice <= exercises.Count:
-                    exercises[choice - 1].Run();
+                    var excercise = exercises[choice - 1];
+
+                    IViewFragment header = new Header(excercise.Title, excercise.Description);
+                    header.Render();
+
+                    excercise.Run();
                     break;
                 default:
-                    Console.WriteLine("Felaktig val. välj 0-{exercises.Count}.");
+                    Console.WriteLine($"Felaktig val. välj 0-{exercises.Count}.");
                     break;
             }
         }
