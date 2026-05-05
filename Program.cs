a51b2277b84c96197acb413324063d361d951ea3
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 17:19:49 +0200

    refactor: integrate Menu class to Main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Ovn2_FlowControl
 {
@@ -14,36 +13,7 @@ namespace Ovn2_FlowControl
                 new RepeatTextExercise(),
                 new ThirdWordExercise()
             };
-
-            bool running = true;
-
-            while (running)
-            {
-                Console.WriteLine("Välkommen till huvudmenyn.");
-                Console.WriteLine("Skriv en siffra för att välja funktion. Skriv 0 för att avsluta.");
-                for (int i = 0; i < exercises.Count; i++)
-                    Console.WriteLine($"{i + 1} →  {exercises[i].Title}");
-
-                Console.Write("> ");
-
-                string? input = Console.ReadLine();
-
-                if (input == "0")
-                {
-                    running = false;
-                    Console.WriteLine("Programmet avslutas.");
-                }
-                else if (int.TryParse(input, out int choice) && choice >= 0 && choice < exercises.Count)
-                {
-                    exercises[choice - 1].Run();
-                }
-                else
-                {
-                    Console.WriteLine("Felaktig input.");
-                }
-
-                Console.WriteLine();
-            }
+            new Menu(exercises).Run();
         }
     }
 }
