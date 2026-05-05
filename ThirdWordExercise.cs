463b2e6ad5560001a73207e4de01300158ff6307
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 15:37:39 +0200

    fix: correct title of the third exercise

--- ThirdWordExercise.cs
+++ ThirdWordExercise.cs
@@ -4,6 +4,6 @@ namespace Ovn2_FlowControl;
 
 public class ThirdWordExercise : IExcercise
 {
-    public string Title => "What's the Word?";
+    public string Title => "What's the Third Word?";
     public void Run() => throw new System.NotImplementedException();
 }
