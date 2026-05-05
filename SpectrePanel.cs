b82ba33b361147e23903ae8b36437d04e150c150
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-05 21:01:09 +0200

    prep: add SpectreHeader class for displaying

---
+++ SpectrePanel.cs
@@ -0,0 +1,28 @@
+using Spectre.Console;
+
+namespace Ovn2_FlowControl;
+
+public class SpectrePanel
+{
+    class SpectreHeader : IViewFragment
+    {
+        private readonly string _title;
+        private readonly string _description;
+        
+        public SpectreHeader(string title, string description)
+        {
+            _title = title;
+            _description = description;
+        }
+        
+        public void Render()
+        {
+            AnsiConsole.Write(
+                new Panel(Markup.Escape(_description))
+                {
+                    Header = new PanelHeader(Markup.Escape(_title))
+                });
+            AnsiConsole.WriteLine();
+        }
+    }
+}
