cccbb8d5526ed07f8aa05914790b22a5be6ef64c
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-06 11:45:58 +0200

    feat: integrate localization into ExerciseBase class
    - Excercise now accept resource key
    - Resolves Title/Description via Loc

--- Exercises/ExerciseBase.cs
+++ Exercises/ExerciseBase.cs
@@ -1,18 +1,22 @@
 using Ovn2_FlowControl.Contracts;
+using Ovn2_FlowControl.Localization;
 
 namespace Ovn2_FlowControl.Exercises;
 
 public abstract class ExerciseBase : IExercise
 {
-    public string Title { get; }
-    public string Description { get; }
+    private readonly string _titleKey;
+    private readonly string _descriptionKey;
 
-    protected ExerciseBase(string title, string description)
+    protected ExerciseBase(string titleKey, string descriptionKey)
     {
-        Title = title;
-        Description = description;
+        _titleKey = titleKey;
+        _descriptionKey = descriptionKey;
     }
 
+    public string Title => Loc.Get(_titleKey);
+    public string Description => Loc.Get(_descriptionKey);
+    
     public abstract void Run(IConsoleAdapter console);
     public override string ToString() => $"{Title} - {Description}";
 }
